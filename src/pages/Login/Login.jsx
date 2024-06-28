bd4367901931c67a88bde0e9f240c7ea423e184c
Winterson <u19cse1028@cit.ac.in>
2024-06-28 22:45:06 +0530

    added login handler to handle form data and backend call

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -13,68 +13,98 @@ import backdrop from "../../assets/ArtBack.svg";
 
 // import styles
 import styles from "./Login.module.css";
+import { useState } from "react";
 
 const Login = () => {
-  let navigate = useNavigate();
+	const [formData, setFormData] = useState({ email: "", password: "" });
+	const navigate = useNavigate();
+	const handleChange = (event) => {
+		setFormData({
+			...formData,
+			[event.target.name]: event.target.value,
+		});
+	};
+	const handleSubmit = async () => {
+		console.log(formData);
+	};
+	function handleRegister() {
+		navigate("/register");
+	}
 
-  function handleRegister() {
-    navigate("/register");
-  }
+	return (
+		<>
+			<div className={styles.container}>
+				<div className={styles.leftContainer}>
+					<div className={styles.artContainer}>
+						<img className={styles.art} src={artwork} alt="artwork" />
+						<img className={styles.backdrop} src={backdrop} alt="backdrop" />
+					</div>
+					<div className={styles.welcomeText}>
+						<p>Welcome aboard my friend</p>
+						<p>just a couple of clicks and we start</p>
+					</div>
+				</div>
 
-  return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.leftContainer}>
-          <div className={styles.artContainer}>
-            <img className={styles.art} src={artwork} alt="artwork" />
-            <img className={styles.backdrop} src={backdrop} alt="backdrop" />
-          </div>
-          <div className={styles.welcomeText}>
-            <p>Welcome aboard my friend</p>
-            <p>just a couple of clicks and we start</p>
-          </div>
-        </div>
-
-        <div className={styles.rightContainer}>
-          <div className={styles.title}>Login</div>
-          <div className={styles.form}>
-            <div className={styles.formContainer}>
-              <div className={styles.inputContainer}>
-                <div className={styles.iconLeft}>
-                  <MdOutlineMailOutline />
-                </div>
-                <input type="text" placeholder="Email" />
-                <div className={styles.iconRight}>
-                  <BiShow style={{ color: "white" }} />
-                </div>
-              </div>
-              <div className={styles.inputContainer}>
-                <div className={styles.iconLeft}>
-                  <HiOutlineLockClosed />
-                </div>
-                <input type="password" placeholder="Password" />
-                <div className={styles.iconRight}>
-                  {/* <BiHide /> */}
-                  <BiShow />
-                </div>
-              </div>
-              <div className={styles.buttonContainer}>
-                <button className={styles.loginBtn}>Login</button>
-              </div>
-            </div>
-          </div>
-          <div className={styles.registerSection}>
-            <p>Have no account yet?</p>
-            <div className={styles.buttonContainer}>
-              <button className={styles.registerBtn} onClick={handleRegister}>
-                Register
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+				<div className={styles.rightContainer}>
+					<div className={styles.title}>Login</div>
+					<div className={styles.form}>
+						<div className={styles.formContainer}>
+							<div className={styles.inputContainer}>
+								<div className={styles.iconLeft}>
+									<MdOutlineMailOutline />
+								</div>
+								<input
+									type="text"
+									name="email"
+									placeholder="Email"
+									onChange={handleChange}
+								/>
+								<div className={styles.iconRight}>
+									<BiShow style={{ color: "white" }} />
+								</div>
+							</div>
+							<div className={styles.inputContainer}>
+								<div className={styles.iconLeft}>
+									<HiOutlineLockClosed />
+								</div>
+								<input
+									type="password"
+									name="password"
+									placeholder="Password"
+									onChange={handleChange}
+								/>
+								<div className={styles.iconRight}>
+									{/* <BiHide /> */}
+									<BiShow />
+								</div>
+							</div>
+							<div className={styles.buttonContainer}>
+								<button
+									type="button"
+									className={styles.loginBtn}
+									onClick={handleSubmit}
+								>
+									Login
+								</button>
+							</div>
+						</div>
+					</div>
+					<div className={styles.registerSection}>
+						<p>Have no account yet?</p>
+						<div className={styles.buttonContainer}>
+							<button
+								type="button"
+								className={styles.registerBtn}
+								onClick={handleRegister}
+							>
+								Register
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Login;
