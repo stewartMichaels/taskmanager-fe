a5166267a17c2894a31fa714ce21d9b6488e5aca
Winterson <u19cse1028@cit.ac.in>
2024-07-02 20:53:27 +0530

    modified Board page

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -15,43 +15,43 @@ import add from "../../assets/board/add-people.svg";
 import styles from "./Board.module.css";
 
 const Board = () => {
-  return (
-    <>
-      <Header />
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <div className={styles.title}>
-            <p>Board</p>
-            <button className={styles.addButton}>
-              <img src={add} alt="Add People" />
-              Add People
-            </button>
-          </div>
+	return (
+		<>
+			<Header />
+			<div className={styles.container}>
+				<div className={styles.header}>
+					<div className={styles.title}>
+						<p>Board</p>
+						<button type="button" className={styles.addButton}>
+							<img src={add} alt="Add People" />
+							Add People
+						</button>
+					</div>
 
-          <div className={styles.dateModal}>
-            <BoardDateModal />
-          </div>
-        </div>
+					<div className={styles.dateModal}>
+						<BoardDateModal />
+					</div>
+				</div>
 
-        <div className={styles.tasksContainer}>
-          <div className={styles.tasksModals}>
-            <div className={styles.tasksModal}>
-              <BacklogModal />
-            </div>
-            <div className={styles.tasksModal}>
-              <ToDo />
-            </div>
-            <div className={styles.tasksModal}>
-              <InProgress />
-            </div>
-            <div className={styles.tasksModal}>
-              <Done />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+				<div className={styles.tasksContainer}>
+					<div className={styles.tasksModals}>
+						<div className={styles.tasksModal}>
+							<BacklogModal />
+						</div>
+						<div className={styles.tasksModal}>
+							<ToDo />
+						</div>
+						<div className={styles.tasksModal}>
+							<InProgress />
+						</div>
+						<div className={styles.tasksModal}>
+							<Done />
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Board;
